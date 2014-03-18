ddb98e9839800d3a5f1792c05f018796481879af
enuobear <enuobear@gmail.com>
2014-03-18 23:43:20 +0800

    add page

--- finances_01.php
+++ finances_01.php
@@ -1,3 +1,38 @@
 <?php include 'public_header.php'; ?>
-<link rel="stylesheet" href="themes/home.css" type="text/css" />
+<link rel="stylesheet" href="themes/global.css" type="text/css" />
+<div class="path">
+	<div>理财产品 > <a href="">货币基金</a></div>
+</div>
+
+
+<div class="wrap clearfix">
+	<table class="table_list_01" cellspacing="0" cellpadding="0">
+	<tr>
+		<th>基金名称</th>
+		<th>基金代码</th>
+		<th>基金公司</th>
+		<th>万元收益<br>(元/日)</th>
+		<th>
+			年化收益率
+			单日　　      7日　　　  14日　　    35日　　   2个月
+		</th>
+		<th>申购<br>状态</th>
+		<th>赎回<br>状态</th>
+	</tr>
+	<tr>
+		<td><a href="">广发天天红货币</a></td>
+		<td>000389</td>
+		<td>广发基金</td>
+		<td>1.7840<br>1-28</td>
+		<td>6.891%</td>
+		<td>5.821%</td>
+		<td>5.821%</td>
+		<td>5.821%</td>
+		<td>5.821%</td>
+		<td></td>
+		<td></td>
+		<td><a href="">去投资</a></td>
+	</tr>
+	</table>
+</div>
 <?php include 'public_footer.php'; ?>
