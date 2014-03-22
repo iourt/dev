7dfad61ac39607fd03530a387eb44f8885de7acd
iourt <iourt@gmail.com>
2014-03-22 13:27:11 +0800

    about

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php include 'public_header.php'; ?>
+<link rel="stylesheet" href="themes/global.css" type="text/css" />
+<style type="text/css">
+.header_logo li.menu_about{
+	color:#044b9d
+}
+</style>
+
+<div class="wrap clearfix">
+	<div>关于我们</div>
+	<div>
+	</div>
+</div>
+<?php include 'public_footer.php'; ?>
