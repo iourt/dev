4ca637a535152cebb2b00e75760f57ccbc2c4a6d
iourt <appuebear@gmail.com>
2014-08-19 23:16:24 +0800

    1

--- pc_footer.php
+++ pc_footer.php
@@ -4,42 +4,25 @@
     <div class="footer">
         <div class="this_left">&copy; 2014 SHANGHAIJAZZWEEK.COM</div>
         <div class="this_mid">
-<!--             <div>
-                <object
-                    classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
-                    codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0"
-                    width="360"
-                    height="26"
-                >
-                    <param name="movie" value="/themes/music/Mr.x.swf" />
-                    <param name="quality" value="high" />
-                    <param name="allowScriptAccess" value="always" />
-                    <param name="wmode" value="transparent">
-                    <embed
-                        src="/themes/music/Mr.x.swf"
-                        quality="high"
-                        type="application/x-shockwave-flash"
-                        WMODE="transparent"
-                        width="360"
-                        height="26"
-                        pluginspage="http://www.macromedia.com/go/getflashplayer"
-                        allowScriptAccess="always"
-                    />
-                </object>
-            </div> -->
- 
             <script src="themes/build/mediaelement-and-player.min.js"></script>
             <link rel="stylesheet" href="themes/build/mediaelementplayer.min.css" />
-            <audio id="player" src="" type="audio/mp3" controls="controls"></audio>
-
+            <div class="this_audio"><audio id="player" src="themes/music/1.mp3" type="audio/mp3" controls="controls"></audio></div>
+            <div class="js_music_play this_play"></div>
         </div>
-        <div class="this_right">Dee Dee Brigewater-Dear Ella</div>
+        <div class="js_music_title this_right"></div>
     </div>
 
     <script type="text/javascript" src="themes/js/pc_common.js"></script>
     <script type="text/javascript">
         siteCommon.setMusic([
-            'themes/music/1.mp3'
+            {
+                src: 'themes/music/1.mp3',
+                title: '第一首'
+            },
+            {
+                src: 'themes/music/2.mp3',
+                title: '第二首'
+            }
         ]);
     </script>
 </body>
--- themes/js/pc_common.js
+++ themes/js/pc_common.js
@@ -35,42 +35,32 @@ var siteCommon = {
     setMusic: function(params) {
         // $('audio,video').mediaelementplayer();
 
+        var detail = params ? params : [],
+            num = detail.length,
+            current = 0,
+            isClick = false;
+
+        var $title = $('.js_music_title');
+
+        if (current == 0) {
+            $title.html(detail[current].title);
+        }
+
         var player = new MediaElement('player', {
-            // shows debug errors on screen
-            enablePluginDebug: false,
-            // remove or reorder to change plugin priority
-            // plugins: ['flash','silverlight'],
-            // specify to force MediaElement to use a particular video or audio type
-            type: '',
-            // path to Flash and Silverlight plugins
-            // pluginPath: '/myjsfiles/',
-            // name of flash file
-            // flashName: 'flashmediaelement.swf',
-            // name of silverlight file
-            silverlightName: 'silverlightmediaelement.xap',
-            // default if the <video width> is not specified
-            defaultVideoWidth: 480,
-            // default if the <video height> is not specified     
-            defaultVideoHeight: 270,
-            // overrides <video width>
-            pluginWidth: -1,
-            // overrides <video height>       
-            pluginHeight: -1,
-            // rate in milliseconds for Flash and Silverlight to fire the timeupdate event
-            // larger number is less accurate, but less strain on plugin->JavaScript bridge
-            timerRate: 250,
-            // method that fires when the Flash or Silverlight object is ready
             success: function (mediaElement, domObject) {
                 // add event listener
-                mediaElement.addEventListener('play', function(e) {
-                    $('.footer .this_right').html("1");
-                }, false);
                 mediaElement.addEventListener('ended', function(e) {
-                    $('.footer .this_right').html("2");
+                    if (current < (num-1)){
+                        current++;
+                    } else {
+                        current = 0;
+                    }
+
+                    mediaElement.setSrc(detail[current].src);
+                    mediaElement.play();
+
+                    $title.html(detail[current].title);
                 }, false);
-                 
-                // call the play method
-                mediaElement.play();
                  
             },
             // fires when a problem is detected
@@ -79,7 +69,16 @@ var siteCommon = {
             }
         });
 
-        player.setSrc('themes/music/1.mp3');
+        $('.js_music_play').on('click', function(){
+            if (isClick) {
+                player.pause();
+                $(this).removeClass('current');
+            } else {
+                player.play();
+                $(this).addClass('current');
+            }
+            isClick = !isClick;
+        });
     }
 };
 
