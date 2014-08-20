5133c7d5252fc148716fbbee6e8b055cee3c0d1d
iourt <appuebear@gmail.com>
2014-08-20 21:57:27 +0800

    1

--- pc_footer.php
+++ pc_footer.php
@@ -11,6 +11,14 @@
             <div class="js_music_play this_play"></div>
         </div>
         <div class="js_music_title this_right"></div>
+        <div class="this_link">
+            <a href=""><img src="themes/link/sns_douban.png" alt="" /></a>
+            <a href=""><img src="themes/link/sns_facebook.png" /></a>
+            <a href=""><img src="themes/link/sns_instagram.png" /></a>
+            <a href=""><img src="themes/link/sns_qq.png" /></a>
+            <a href=""><img src="themes/link/sns_sina.png" /></a>
+            <a href=""><img src="themes/link/sns_twitter.png" /></a>
+        </div>
     </div>
 
     <script type="text/javascript" src="themes/js/pc_common.js"></script>
