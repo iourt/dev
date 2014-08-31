88ea3c7b357e795a02f98a9af07d2cf77e8e36bc
iourt <appuebear@gmail.com>
2014-08-31 19:08:19 +0800

    1

---
+++ pc_05.php
@@ -0,0 +1,89 @@
+<?php include 'pc_header.php'; ?>
+<div class="img_01">
+    <img src="/themes/temp/6.jpg" alt="" />
+</div>
+
+<div class="other clearfix">
+    <div class="this_left">
+        <div class="this_title">
+            Ticket<br>
+            票务
+            <div>
+                <span class="stand">&nbsp;</span>
+                站票
+                <span class="seat">&nbsp;</span>
+                全票
+            </div>
+        </div>
+
+        <ul class="this_list">
+            <li>
+                <div>
+                    10/2
+                    14:00
+                </div>
+                <div>
+                    <span class="stand">STAND</span>
+                    RMB 150
+                </div>
+                <div>
+                    <span class="seat">SEAT</span>
+                    RMB 200
+                </div>
+            </li>
+            <li class="this_line">
+                <div>
+                    10/2
+                    14:00
+                </div>
+                <div>
+                    <span class="stand">STAND</span>
+                    RMB 150
+                </div>
+                <div>
+                    <span class="seat">SEAT</span>
+                    RMB 200
+                </div>
+            </li>
+            <li>
+                <div>
+                    10/2
+                    14:00
+                </div>
+                <div>
+                    <span class="stand">STAND</span>
+                    RMB 150
+                </div>
+                <div>
+                    <span class="seat">SEAT</span>
+                    RMB 200
+                </div>
+            </li>
+            <li class="this_line">
+                <div>
+                    All Day 全日通票
+                </div>
+                <div>
+                    <span class="stand">STAND</span>
+                    RMB 150
+                </div>
+                <div>
+                    <span class="seat">SEAT</span>
+                    RMB 200
+                </div>
+            </li>
+        </ul>
+
+        <div class="this_bug">BUY 购票</div>
+    </div>
+
+
+
+    <div class="this_right">
+        asdfasdfas<br>
+        asdfasdfas<br>
+        asdfasdfas<br><br>
+        asdfasdfas<br>
+    </div>
+</div>
+<?php include 'pc_footer.php'; ?>
---
+++ pc_06.php
@@ -0,0 +1,64 @@
+<?php include 'pc_header.php'; ?>
+<div class="img_01">
+    <img src="/themes/temp/6.jpg" alt="" />
+</div>
+
+<div class="other clearfix">
+    <div class="this_left">
+        <div class="this_title">
+            Vip Gate<br>
+            贵宾注册
+        </div>
+
+        <div style="padding:20px 0 30px 0;line-height:20px;">
+            阿斯蒂芬阿斯蒂芬阿萨德发生的发送到飞洒地方是打发撒地方
+        </div>
+
+        <ul class="this_reg">
+            <li>
+                Name 姓名 <input >
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                Sex 性别
+
+                <select>
+                    <option>Man 男</option>
+                    <option>Women 女</option>
+                </select>
+            </li>
+            <li>
+                E-mail 邮箱地址
+                <input >
+            </li>
+            <li>
+                City 所在城市
+                <select>
+                    <option>Shanghai 上海</option>
+                    <option>Shanghai 上海</option>
+                </select>
+            </li>
+            <li>
+                Mobile 手机号码
+                <input >
+            </li>
+            <li>
+                <em class="this_gray">阿斯蒂芬</em>
+                Code 验证码
+                <input style="width:60px;" >
+                <em class="this_gray">打的</em>
+            </li>
+            <li>
+                <em class="this_yellow">OK 提交</em>
+            </li>
+        </ul>
+    </div>
+
+
+
+    <div class="this_right">
+        asdfasdfas<br>
+        asdfasdfas<br>
+        asdfasdfas<br><br>
+        asdfasdfas<br>
+    </div>
+</div>
+<?php include 'pc_footer.php'; ?>
