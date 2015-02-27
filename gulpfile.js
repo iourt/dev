988a4c301962111598b70b8e3698458344aa90c9
xiong.shaole <slxiong@ctrip.com>
2015-02-27 17:19:40 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,17 @@
 var fs           = require('fs'),
 	argv         = require('yargs').argv,
 	os           = require('os'),
-	build        = require('./tools/build.js'),
-	movefiles    = require('./tools/move-files.js'),
 
 	gulp 		 = require('gulp'),
 	shell        = require('gulp-shell'),
-	rjs          = require('gulp-requirejs'),
-	uglify       = require('gulp-uglify'),
 	rename       = require('gulp-rename'),
-	handlebars   = require('gulp-handlebars'),
 	wrap         = require('gulp-wrap'),
 	clean 		 = require('gulp-clean'),
 	sass         = require('gulp-sass'),
 	concat       = require('gulp-concat'),
 	connect 	 = require('gulp-connect'),
 	replace      = require('gulp-replace'),
-	minifycss    = require('gulp-minify-css'),
-	declare      = require('gulp-declare'),
-	minifyHTML   = require('gulp-minify-html'),
-	defineModule = require('gulp-define-module');
+	declare      = require('gulp-declare');
 
 var dev = argv.dev,
 	buildpath = './build/';
@@ -29,270 +21,21 @@ if (dev == 'cp') {
 }
 
 var task = {
-	concat: function() {
-		gulp.src('./lib/**/*.js')
-			.pipe(concat('lib.pred.js'))
-			.pipe(gulp.dest('./source/js/lib'))
-	},
-
-
-	rjs: function() {
-	    rjs({
-	        baseUrl: './lib',
-	        out: 'js/lib.code.js',
-	        include: [
-	        	'jquery', 
-	        	'underscore', 
-	        	'backbone',
-	        	'handlebars',
-	        	'requirejs',
-	        	'router',
-	        	'lib.config',
-	        	'Common'
-	        ],
-		    paths: {
-		        'jquery':     'code/jquery-2.1.3.min',
-		        'underscore': 'code/underscore-min-1.7.0',
-		        'backbone':   'code/backbone-min-1.1.2',
-		        'handlebars': 'code/handlebars-v2.0.0',
-		        'requirejs':  'code/require-2.1.15',
-		        'router':     '../source/js/Common/Router',
-		        'Common':     '../source/js/Common/Common'
-		    },
-	        shim: {
-		    	backbone: {
-		    		'deps': ['jquery', 'underscore'],
-		    		'exports': 'Backbone'
-		    	},
-		    	underscore: {
-		    		'exports': '_'
-		    	},
-		        handlebars: {
-		            exports: 'Handlebars',
-
-		            init: function() {
-
-		                this.Handlebars = Handlebars;
-
-		                return this.Handlebars;
-		                
-		            }
-		        }
-	        }
-	        // ... more require.js options 
-	    })
-		.pipe(uglify({outSourceMap: false}))
-		.pipe(gulp.dest('./source/')); // pipe it to the output DIR 
-	},
-	move: function() {
-		var d = new Date(),
-			version = d.getTime();
-
-		gulp.src('source/**/*.html')
-			.pipe(replace(/\.css/g, '.css?v='+version))
-			.pipe(replace(/\.js/g, '.js?v='+version))
-			.pipe(gulp.dest(buildpath));
-
-		gulp.src([
-				'source/**/*.js',
-				'!source/js/App.js',
-				'!source/js/Config.js',
-				'!source/js/Page/*.js',
-				'!source/js/Common/Router.js',
-				'!source/js/Common/Common.js',
-				'!source/js/View/*.js',
-				'!source/js/Layout/*.js',
-				'!source/js/Model/*.js'
-			])
-			.pipe(uglify({outSourceMap: false}))
-			.pipe(gulp.dest(buildpath));
-
-		gulp.src('source/**/*.css')
-			.pipe(minifycss())
-			.pipe(gulp.dest(buildpath));
-		
-		gulp.src([
-				'source/**/*.jpg',
-				'source/**/*.png'
-			])
-			.pipe(gulp.dest(buildpath));
-	},
-
-	templates: function() {
-		gulp.src('./source/**/*.hbs')
-			.pipe(handlebars())
-			.pipe(defineModule('amd'))
-			.pipe(movefiles(function(path){
-				// path.dirname = strpath;
-				path.basename += '.hbs';
-				path.extname = '.js'
-			}))
-			.pipe(gulp.dest(''))
-	},
-
 	sass: function(type) {
-		var filePath = './source/';
 
-		if (type == 'build') {
+		gulp.src('./source/themes/all.scss')
+			.pipe(sass())
+			.pipe(gulp.dest('build/themes'));
 
-			filePath = buildpath;
-			gulp.src('./source/themes/all.scss')
-				.pipe(sass())
-				.pipe(minifycss())
-				.pipe(gulp.dest(filePath + 'themes'));
-
-		} else {
-
-			gulp.src('./source/themes/all.scss')
-				.pipe(sass())
-				.pipe(gulp.dest(filePath + 'themes'));
-
-		}
 	},
+	
+	move: function() {
 
-	createFrame: function(type) {
-		var filePath = './source/',
-			fileName = 'js/Frame.js';
-
-		if (type == 'build') {
-			filePath = buildpath;
-			fileName = 'js/lib.frame.js';
-		}
-
-	    rjs({
-	        baseUrl: './lib',
-	        out: fileName,
-	        include: [
-	        	'zepto', 
-	        	'underscore', 
-	        	'backbone',
-	        	'handlebars',
-	        	'requirejs',
-	        	'lib.config'
-	        ],
-		    paths: {
-		        'zepto':      'code/zepto.min-1.1.6',
-		        'underscore': 'code/underscore-min-1.7.0',
-		        'backbone':   'code/backbone-min-1.1.2',
-		        'handlebars': 'code/handlebars-v2.0.0',
-		        'requirejs':  'code/require-2.1.15'
-		    },
-	        shim: {
-	        	zepto: {
-	        		'exports': '$'
-	        	},
-		    	backbone: {
-		    		'deps': ['zepto', 'underscore'],
-		    		'exports': 'Backbone'
-		    	},
-		    	underscore: {
-		    		'exports': '_'
-		    	},
-		        handlebars: {
-		            exports: 'Handlebars',
-
-		            init: function() {
-
-		                this.Handlebars = Handlebars;
-
-		                return this.Handlebars;
-		                
-		            }
-		        }
-	        }
-	        // ... more require.js options 
-	    })
-		.pipe(uglify({outSourceMap: false}))
-		.pipe(gulp.dest(filePath)); // pipe it to the output DIR 
-	},
-
-	createRouter: function() {
-		var strpath = {
-	        'zepto':      'empty:',
-	        'underscore': 'empty:',
-	        'backbone':   'empty:',
-	        'handlebars': 'empty:',
-	        'require':    'empty:',
-
-	        'router': 'js/Common/Router',
-	        'Common': 'js/Common/Common'
-		};
-
-	    rjs({
-	        baseUrl: './source/',
-	        out: 'js/lib.common.js',
-	        include: [
-	        	'router', 
-	        	'Common'
-	        ],
-		    paths: strpath
-	    })
-		.pipe(uglify({outSourceMap: false}))
-		.pipe(gulp.dest(buildpath));
-	},
-
-	createConfig: function() {
-		gulp.src([
-				'source/js/App.js',
-				'source/js/Config.js'
-			])
-			.pipe(uglify({outSourceMap: false}))
-			.pipe(concat('lib.config.js'))
-			.pipe(gulp.dest(buildpath + 'js/'))
-	},
-
-	minrjs: function() {
-		var strpath = {
-	        'zepto':      'empty:',
-	        'underscore': 'empty:',
-	        'backbone':   'empty:',
-	        'handlebars': 'empty:',
-	        'require':    'empty:',
-
-	        'router':     'empty:',
-	        'Common':     'empty:'
-		};
-
-		var files = [
-			'Page/List',
-			'Page/Add',
-			'Page/Comment',
-			'Page/Detail',
-			'Page/Login'
-		];
-
-		files.forEach(function(name) {
-		    rjs({
-		        baseUrl: './source/js',
-		        out: name + '.js',
-		        include: [name],
-			    paths: strpath
-		    })
-			.pipe(uglify({outSourceMap: false}))
-			.pipe(gulp.dest(buildpath + 'js'));
-		});
-	},
-
-	moveFile: function() {
-		var d = new Date(),
-			version = d.getTime(),
-			opts = {comments:true,spare:true};
-
-		// 移动HTML文件
-		gulp.src('source/**/*.html')
-			.pipe(replace(/\.css/g, '.css?v='+version))
-			.pipe(replace(/Frame\.js/g, 'lib.frame.js'))
-			.pipe(replace(/Config\.js/g, 'lib.config.js'))
-			.pipe(replace(/App\.js/g, 'lib.common.js'))
-			.pipe(minifyHTML(opts))
-			.pipe(gulp.dest(buildpath));
-		
-		// 移动图片文件
 		gulp.src([
 				'source/**/*.jpg',
 				'source/**/*.png'
 			])
-			.pipe(gulp.dest(buildpath));
+			.pipe(gulp.dest('build/'));
 
 	},
 
@@ -324,7 +67,6 @@ var task = {
     watch: function() {
     	var self = this;
     	gulp.watch([
-    		'./source/**/*.hbs',
     		'./source/**/*.scss'
     	], function(){
     		self.reload();
@@ -343,22 +85,9 @@ var task = {
 };
 
 
-gulp.task('templates', function() {
-	task.templates();
-});
-
-gulp.task('frame', function(){
-	task.createFrame();
-});
-
-
 gulp.task('clean', function() {
 	var strpath = [
 			'./build/'
-			// './source/**/*.hbs.js'
-			// './source/js/lib.code.js',
-			// './source/js/lib.config.js',
-			// './source/**/*.min.js'
 		];
 
 	return gulp.src(strpath, {read: false})
@@ -368,20 +97,7 @@ gulp.task('clean', function() {
 
 
 gulp.task('run', ['clean'], function() {
-	task.templates();
-	task.createFrame();
 	task.sass();
 	task.connect('source');
 	task.watch();
-});
-
-gulp.task('build', ['clean', 'templates'], function(){
-	task.createFrame('build');
-	task.createConfig();
-	task.createRouter();
-	task.sass('build');
-	task.minrjs();
-	task.moveFile();
-
-	if (dev != 'cp') task.connect('build');
-});
+);
