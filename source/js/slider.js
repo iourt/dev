fbe5b322efd8b9619d19f07e91a64fe40f641673
xiong.shaole <slxiong@ctrip.com>
2015-03-02 19:21:23 +0800

    modify

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,15 +1,15 @@
 var Slider = function(){};
 
 Slider.prototype = {
-    init: function(dom){
+    init: function(params){
         var self = this;
 
-        self.nav = dom;
-        self.$slider = $(self.nav);
-        self.$dom = $(self.nav+' ul');
+        self.$slider = $(params.elem).eq(params.z);
+
+        self.$dom = self.$slider.find('ul');
         
-        self.w = $(self.nav+' li').width();
-        self.len = $(self.nav+' li').length;
+        self.w = self.$slider.find('li').width();
+        self.len = self.$slider.find('li').length;
 
         self.config();
 
@@ -28,9 +28,9 @@ Slider.prototype = {
         var self = this;
 
         var str = '';
-            $html = $(self.nav+' li').eq(0).clone();
+            $html = self.$slider.find('li').eq(0).clone();
 
-        $(self.nav+' ul').append($html);
+        self.$slider.find('ul').append($html);
 
         for (var i=0; i<self.len; i++) {
             str += '<div></div>';
@@ -38,7 +38,7 @@ Slider.prototype = {
 
         self.$slider.append('<div class="this_icon">'+str+'</div>');
         
-        $(self.nav+' .this_icon div').eq(0).addClass('current');
+        self.$slider.find('.this_icon div').eq(0).addClass('current');
     },
 
     _move: function(){
@@ -78,7 +78,92 @@ Slider.prototype = {
 
     _icon: function(num){
         var self = this;
-        $(self.nav+' .this_icon div').removeClass('current');
-        $(self.nav+' .this_icon div').eq(num).addClass('current');
+        self.$slider.find('.this_icon div').removeClass('current');
+        self.$slider.find('.this_icon div').eq(num).addClass('current');
     }
 }
+
+// var Slider = function(){};
+
+// Slider.prototype = {
+//     init: function(dom){
+//         var self = this;
+
+//         self.nav = dom;
+//         self.$slider = $(self.nav);
+//         self.$dom = $(self.nav+' ul');
+        
+//         self.w = $(self.nav+' li').width();
+//         self.len = $(self.nav+' li').length;
+
+//         self.config();
+
+//         self._html();
+
+//         self._loop();
+//     },
+
+//     config: function(){
+//         var self = this;
+
+//         self.current = 1;
+//     },
+
+//     _html: function(){
+//         var self = this;
+
+//         var str = '';
+//             $html = $(self.nav+' li').eq(0).clone();
+
+//         $(self.nav+' ul').append($html);
+
+//         for (var i=0; i<self.len; i++) {
+//             str += '<div></div>';
+//         }
+
+//         self.$slider.append('<div class="this_icon">'+str+'</div>');
+        
+//         $(self.nav+' .this_icon div').eq(0).addClass('current');
+//     },
+
+//     _move: function(){
+//         var self = this;
+
+//         self.$dom.css({
+//             '-webkit-transform': 'translate3d(-'+ self.w*self.current +'px, 0, 0)',
+//             '-webkit-transition': '-webkit-transform 1s ease-in-out'
+//         });
+
+//         self._icon(self.current);
+        
+//         self.$dom.on('webkitTransitionEnd', function (e) {
+//             if(self.current == self.len+1) {
+//                 $(this).attr('style', '');
+//                 self.current = 1;
+//                 self.icon(0);
+//             }
+//         });
+
+//         self.current++;
+
+//         if (self.current == self.len+1){
+//             self._icon(0);
+//         }
+
+//         self._loop();
+//     },
+
+//     _loop: function(){
+//         var self = this;
+
+//         setTimeout(function(){
+//             self._move();
+//         }, 4000);
+//     },
+
+//     _icon: function(num){
+//         var self = this;
+//         $(self.nav+' .this_icon div').removeClass('current');
+//         $(self.nav+' .this_icon div').eq(num).addClass('current');
+//     }
+// }
