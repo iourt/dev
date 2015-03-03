882d4f2f44f9a750a0a4bec8dfd8fef608b32d4e
xiong.shaole <slxiong@ctrip.com>
2015-03-03 18:28:01 +0800

    modify

--- source/js/slider.js
+++ source/js/slider.js
@@ -211,107 +211,90 @@ Slider.prototype = {
 
 
 
+var SliderMove = function(){};
 
+SliderMove.prototype = {
+    init: function(params){
+        var self = this;
 
+        self.$slider = $(params.elem).eq(params.z);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var Slider = function(){};
-
-// Slider.prototype = {
-//     init: function(params){
-//         var self = this;
-
-//         self.$slider = $(params.elem).eq(params.z);
-
-//         self.$dom = self.$slider.find('ul');
+        self.$dom = self.$slider.find('ul');
         
-//         self.w = self.$slider.find('li').width();
-//         self.len = self.$slider.find('li').length;
+        self.w = self.$slider.find('li').width();
+        self.len = self.$slider.find('li').length;
 
-//         self.config();
+        self.config();
 
-//         self._html();
+        self._html();
 
-//         self._loop();
-//     },
+        self._loop();
+    },
 
-//     config: function(){
-//         var self = this;
+    config: function(){
+        var self = this;
 
-//         self.current = 1;
-//     },
+        self.current = 1;
+    },
 
-//     _html: function(){
-//         var self = this;
+    _html: function(){
+        var self = this;
 
-//         var str = '';
-//             $html = self.$slider.find('li').eq(0).clone();
+        var str = '';
+            $html = self.$slider.find('li').eq(0).clone();
 
-//         self.$slider.find('ul').append($html);
+        self.$slider.find('ul').append($html);
 
-//         for (var i=0; i<self.len; i++) {
-//             str += '<div></div>';
-//         }
+        for (var i=0; i<self.len; i++) {
+            str += '<div></div>';
+        }
 
-//         self.$slider.append('<div class="this_icon">'+str+'</div>');
+        self.$slider.append('<div class="this_icon">'+str+'</div>');
         
-//         self.$slider.find('.this_icon div').eq(0).addClass('current');
-//     },
+        self.$slider.find('.this_icon div').eq(0).addClass('current');
+    },
 
-//     _move: function(){
-//         var self = this;
+    _move: function(){
+        var self = this;
 
-//         self.$dom.css({
-//             '-webkit-transform': 'translate3d(-'+ self.w*self.current +'px, 0, 0)',
-//             '-webkit-transition': '-webkit-transform 1s ease-in-out'
-//         });
+        self.$dom.css({
+            '-webkit-transform': 'translate3d(-'+ self.w*self.current +'px, 0, 0)',
+            '-webkit-transition': '-webkit-transform 1s ease-in-out'
+        });
 
-//         self._icon(self.current);
+        self._icon(self.current);
         
-//         self.$dom.on('webkitTransitionEnd', function (e) {
-//             if(self.current == self.len+1) {
-//                 $(this).attr('style', '');
-//                 self.current = 1;
-//                 self.icon(0);
-//             }
-//         });
+        self.$dom.on('webkitTransitionEnd', function (e) {
+            if(self.current == self.len+1) {
+                $(this).attr('style', '');
+                self.current = 1;
+                self._icon(0);
+            }
+        });
 
-//         self.current++;
+        self.current++;
 
-//         if (self.current == self.len+1){
-//             self._icon(0);
-//         }
+        if (self.current == self.len+1){
+            self._icon(0);
+        }
 
-//         self._loop();
-//     },
+        self._loop();
+    },
 
-//     _loop: function(){
-//         var self = this;
+    _loop: function(){
+        var self = this;
 
-//         setTimeout(function(){
-//             self._move();
-//         }, 4000);
-//     },
+        setTimeout(function(){
+            self._move();
+        }, 4000);
+    },
 
-//     _icon: function(num){
-//         var self = this;
-//         self.$slider.find('.this_icon div').removeClass('current');
-//         self.$slider.find('.this_icon div').eq(num).addClass('current');
-//     }
-// }
+    _icon: function(num){
+        var self = this;
+        self.$slider.find('.this_icon div').removeClass('current');
+        self.$slider.find('.this_icon div').eq(num).addClass('current');
+    }
+}
 
 
 
